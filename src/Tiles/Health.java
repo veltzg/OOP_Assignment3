5231a3320d41d7896ad3fe7071205a7ea1ddfd90
Gili Veltz <veltzg@post/bgu.ac.il>
2022-06-15 19:20:34 +0300

    Setting Business Layer basic classes

---
+++ src/Tiles/Health.java
@@ -0,0 +1,33 @@
+package Tiles;
+
+import Tiles.*;
+import java.util.*;
+
+public class Health {
+    protected int pool;
+    protected int amount;
+
+    public Health(int pool, int amount){
+        this.pool=pool;
+        this.amount=amount;
+    }
+
+    public int getHealthPool() {
+        return pool;
+    }
+    public int getHealthAmount() { return amount;}
+
+    public void setHealthAmount(int health){
+        amount = health;
+        if (amount>pool)
+            amount = pool;
+        else if(amount<0)
+            amount=0;
+    }
+
+    public void setHealthPool(int health){pool = health;}
+
+    public String toString(){
+        return ""+amount+"/"+pool;
+    }
+}
