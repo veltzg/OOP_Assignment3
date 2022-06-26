9d002c9d475cb71489b083014b75365d41ff1c0c
Amit Hassan <amithass@post.bgu.ac.il>
2022-06-26 18:16:49 +0300

    DONT KNOW

--- src/Tiles/Rogue.java
+++ src/Tiles/Rogue.java
@@ -7,11 +7,28 @@ public class Rogue extends Player {
 
     //fields:
     protected Integer cost;
+
+    public void setCurrentEnergy(Integer currentEnergy) {
+        this.currentEnergy = currentEnergy;
+    }
+
     protected Integer currentEnergy;
+    private final Integer MAX_ENERGY = 100;
 
     //constructor:
-    protected Rogue(char tile, String name, Integer healthPool, Integer healthAmount, Integer attackPoints, Integer defensePoints) {
-        super(tile, name, healthPool, healthAmount, attackPoints, defensePoints);
+    protected Rogue(String name, Integer healthCpacity, Integer attackPoints, Integer defensePoints, Integer cost) {
+        super(name, healthCpacity, attackPoints, defensePoints);
+        this.cost = cost;
+        this.currentEnergy = MAX_ENERGY;
+    }
+
+    @Override
+    public void castAbility(List<Enemy> enemies) {
+    }
+
+    @Override
+    public void processStep() {
+        setCurrentEnergy(Math.min(currentEnergy + 10, MAX_ENERGY));
     }
 
     //methods:
